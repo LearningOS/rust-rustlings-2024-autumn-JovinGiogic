3b204eb1b68dc308de1a8bae82063dc9790e2d0e
Your Name <you@example.com>
2024-10-13 19:05:18 +0800

    solve errors, options

--- exercises/options/options3.rs
+++ exercises/options/options3.rs
@@ -3,7 +3,6 @@
 // Execute `rustlings hint options3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 struct Point {
     x: i32,
@@ -13,7 +12,7 @@ struct Point {
 fn main() {
     let y: Option<Point> = Some(Point { x: 100, y: 200 });
 
-    match y {
+    match &y {  // 当不用y的引用，匹配到Some(p)时，y的所有权会被转移到p
         Some(p) => println!("Co-ordinates are {},{} ", p.x, p.y),
         _ => panic!("no match!"),
     }
